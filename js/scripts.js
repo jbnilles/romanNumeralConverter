9cb3d596932d683dc2a5dc40c12889dc5c8d4515
jbnilles <jbnilles24@gmail.com>
2020-08-20 10:51:05 -0700

    test 2 and 3 are satisfied
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,121 @@ $(document).ready(function () {
 
   $('#romanNumeral').submit(function () {
     event.preventDefault();
-
+//user
+    //check
+    //inter
   });
 
 });
 
 
-//  66
-//  LXVI
+//  90
+//LXXXX
+//  XC
 
 function integerToRoman (num) {
-  const ROMAN = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-  const VALUES = [1, 5, 10, 50, 100, 500, 1000];
+  const ROMAN = ['I', 'IV','V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+  const VALUES = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
   let result = [];
-  let i = ROMAN.length - 1;
-  while (num > 0) {
+  for  (i = ROMAN.length - 1; num > 0 && num <= 3999; i--) {
     while ( num >= VALUES[i]){
         result.push(ROMAN[i]);
         num -= VALUES[i];
     }
-    i--;
   }
   return result.join('');
 }
+// function integerToRoman (num) {
+//   const ROMAN = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+//   const VALUES = [1, 5, 10, 50, 100, 500, 1000];
+//   let result = [];
+//   let i = ROMAN.length - 1;
+//   while (num > 0) {
+//     if(num >= 1000) {
+//       for(let j = 0; j < num / 1000; j++) {
+//         result.push('M');
+//         num -= 1000;
+//       }
+//     }
+//     else if (num >= 500) {
+//       if(num >= 900)
+//       {
+//         result.push('CM');
+//         num -= 900;
+//       }
+//       else
+//       {
+//         for(let j = 0; j < num / 500; j++) {
+//           result.push('D');
+//           num -= 500;
+//         }
+//       }
+//     }
+//     else if (num >= 100){
+//       if(num >= 400)
+//       {
+//         result.push('CD');
+//         num -= 400;
+//       }
+//       else {
+//         for(let j = 0; j < num / 100; j++) {
+//           result.push('X');
+//           num -= 100;
+//         }
+//       }
+//     }
+//     else if (num >= 50){
+//       if(num >= 90)
+//       {
+//         result.push('XC');
+//         num -= 90;
+//       }
+//       else {
+//         for(let j = 0; j < num / 50; j++) {
+//           result.push('L');
+//           num -= 50;
+//         }
+//       }
+//     }
+//     else if (num >= 10){
+//       if(num >= 40)
+//       {
+//         result.push('XL');
+//         num -= 40;
+//       }
+//       else {
+//         for(let j = 0; j < num / 10; j++) {
+//           result.push('X');
+//           num -= 10;
+//         }
+//       }
+//     }
+//     else if (num >= 5){
+//       if(num >= 9)
+//       {
+//         result.push('IX');
+//         num -= 9;
+//       }
+//       else {
+//         for(let j = 0; j < num / 5; j++) {
+//           result.push('V');
+//           num -= 5;
+//         }
+//       }
+//     }
+//     else if (num >= 1){
+//       if(num >= 4)
+//       {
+//         result.push('IV');
+//         num -= 4;
+//       }
+//       else {
+//         for(let j = 0; j < num / 1; j++) {
+//           result.push('I');
+//           num -= 1;
+//         }
+//       }
+//     }
+//   }
+//   return result.join('');
+// }
